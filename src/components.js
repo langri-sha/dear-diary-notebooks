afee0ad0fedeb23ec27f1e438a144bc11ef5145f
Filip Dupanović <filip.dupanovic@gmail.com>
2016-08-09 14:18:09 +0200

    feat(web): Add logo

--- src/components.js
+++ src/components.js
@@ -1,9 +1,19 @@
 import React from 'react'
 
+import logo from './images/logo.svg'
 import style from './styles/index.css'
 
 const Content = () => (
-  <article className={style.content} />
+  <article className={style.content}>
+    <header>
+      <h1>
+        <img
+          alt={'Dear Diary Notebooks'}
+          src={logo}
+        />
+      </h1>
+    </header>
+  </article>
 )
 
 const Diary = () => (
